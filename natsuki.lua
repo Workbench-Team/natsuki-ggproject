8080d1b70867b3814c9ca41405eca1ac860c5dae
ProfessorBrain <admin@ggproject.xyz>
2019-12-02 22:25:53 +0300

    Test update

--- natsuki.lua
+++ natsuki.lua
@@ -1,2 +1,8 @@
 discordia = require('discordia')
 client = discordia.Client()
+http = require('coro-http')
+qs = require('querystring')
+json = require('json')
+timer = require('timer')
+discordia.extensions()
+prefix = '.'
