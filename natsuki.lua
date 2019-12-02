34bb7e95987e967bac9a203c772f35daef132b13
ProfessorBrain <admin@ggproject.xyz>
2019-12-02 23:03:21 +0300

    Launch bot

--- natsuki.lua
+++ natsuki.lua
@@ -1,10 +1,20 @@
-discordia = require('discordia')
-client = discordia.Client()
+disc = require('discordia')
+cl = disc.Client()
 http = require('coro-http')
 qs = require('querystring')
 json = require('json')
 timer = require('timer')
-discordia.extensions()
-prefix = '.'
-version = '0.0'
+disc.extensions()
+pref = '.'
+ver = '0.0'
+
+
+
 token = json.decode(io.open("token.json", "r"):read("*a"))["value"]
+cl:run('Bot'..token)
+
+
+
+cl:on('ready' function()
+	print(cl.user.tag..' launched')
+end)
