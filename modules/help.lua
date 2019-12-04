f1fef7b437254b8cc2ffbf61d0f5feb3b6071f4d
ProfessorBrain <admin@ggproject.xyz>
2019-12-04 22:08:32 +0300

    Help added

--- modules/help.lua
+++ modules/help.lua
@@ -1,7 +1,12 @@
 function help(msg)
 	local table = {
-		title = 'Бот в разработке',
-		description = '**[Ссылка на GitHub страницу](https://github.com/ProfessorBrain/natsuki)**'
+		title = '**Бот в разработке**',
+		description = '**[Ссылка на GitHub страницу](https://github.com/ProfessorBrain/natsuki)**',
+		fields = {
+			{name=_G.pref..'help',value='Выводит помощь по командам'},
+			{name=_G.pref..'img',value='NSFW и SFW картинки с сайта nekos.life'},
+			{name=_G.pref..'say[d] <text>',value='Сказать <text>\nd - удалить ваше сообщение'}
+		}
 	}
 	msg:reply { embed = table }
 end
