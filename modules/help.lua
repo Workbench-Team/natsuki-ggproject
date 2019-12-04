4bd9a99f411067719b36da262ea04a2ae623ed17
ProfessorBrain <admin@ggproject.xyz>
2019-12-04 22:10:48 +0300

    Update help

--- modules/help.lua
+++ modules/help.lua
@@ -2,6 +2,14 @@ function help(msg)
 	local table = {
 		title = '**Бот в разработке**',
 		description = '**[Ссылка на GitHub страницу](https://github.com/ProfessorBrain/natsuki)**',
+		author = {
+			icon_url = msg.author.avatarURL,
+			name = msg.author.tag
+		},
+		footer = {
+			icon_url = _G.cl.user.avatarURL,
+			text = _G.cl.user.tag
+		},
 		fields = {
 			{name=_G.pref..'help',value='Выводит помощь по командам'},
 			{name=_G.pref..'img',value='NSFW и SFW картинки с сайта nekos.life'},
