75c9d310ae79bb80d980aadb43629a8ca8ab21d7
ProfessorBrain <admin@ggproject.xyz>
2019-12-05 22:56:09 +0300

    Setup status

--- modules/voice.lua
+++ modules/voice.lua
@@ -1,2 +1,9 @@
-function join()
+function music(msg, args)
 end
+_G.cl:on('messageCreate', function(msg)
+	local cont = msg.content
+	local args = cont:split()
+	if args[1] == _G.pref..'music' then
+		music(msg, args)
+	end
+end)
