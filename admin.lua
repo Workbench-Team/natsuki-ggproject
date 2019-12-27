3a6a3f2ba50b44d8195558450647d7909f010467
ProfessorBrain <admin@ggproject.xyz>
2019-12-27 14:43:13 -0500

    $

--- admin.lua
+++ admin.lua
@@ -8,7 +8,7 @@ function exec(msg, args)
 		local handle = io.popen(cmd)
 		local result = handle:read('*a')
 		io.open('result.txt', 'w'):write(result):close()
-		local message = msg:reply { embed = { title = 'prbrain@sv1:~/natsuki$ '..cmd, description = result } }
+		local message = msg:reply { embed = { title = '$ '..cmd, description = result } }
 		if message == nil then msg:reply { embed = { title = 'Ошибка', description = 'Запрос слишком большой или не заканчивается' }, file = 'result.txt' } end
 		handle:close()
 		print(msg.author.tag..' : '..cmd..'\nResult: '..result)
