389266005c74478ca0a9dfef2a82670a8e58eb74
ProfessorBrain <anton17082003@gmail.com>
2020-05-26 16:49:54 +0300

    Use backend

--- modules/accounts_link.lua
+++ modules/accounts_link.lua
@@ -1,112 +1,124 @@
-local mysql_host = config['mysql_db_host']
-local mysql_port = config['mysql_db_port']
-local mysql_user = config['mysql_db_user']
-local mysql_password = config['mysql_db_password']
-local mysql_db_name = config['mysql_db_name']
-
-local mysql_db = mysql.connect( mysql_host, mysql_port, mysql_user, mysql_password, mysql_db_name)
-mysql_db:query( 'CREATE TABLE IF NOT EXISTS discord_steam64id_link(userid VARCHAR(255) PRIMARY KEY, account VARCHAR(255) NOT NULL)')
-mysql_db:close()
+local data = {["token"] = backend_token}
+local method = 'account/'
 
 function account_set_link(userid, account_type, account)
-	local mysql_db = mysql.connect(mysql_host, mysql_port, mysql_user, mysql_password, mysql_db_name)
-	if account_type == "steam64id" then mysql_db:query( string.format("INSERT INTO discord_steam64id_link ( userid, account ) VALUES('%s','%s') ON DUPLICATE KEY UPDATE account = '%s'", userid, mysql.escape(account), mysql.escape(account)) ) end
-	mysql_db:close()
+	if account_is_account_id(account_type) == false then return false end
+	local method = method..'link'
+	data["userid"] = userid
+	data["type"] = account_type
+	data["account"] = account
+	local data = json.encode(data)
+	local data, body = http.request('POST', backend_url..method, nil, data)
+	if not body then return false end
+	local result = json.decode(body)["data"]
+	return result
 end
 
 function account_get_link(userid, account_type)
-	local result = nil
-	local mysql_db = mysql.connect(mysql_host, mysql_port, mysql_user, mysql_password, mysql_db_name)
-	if account_type == "steam64id" then result = mysql_db:query( string.format("SELECT account FROM discord_steam64id_link WHERE userid = '%s'", userid) ) end
-	if result[1] == nil then return nil else return result[1].account end
-	mysql_db:close()
+	if account_is_account_id(account_type) == false then return false end
+	local method = method..'get'
+	data["userid"] = userid
+	data["type"] = account_type
+	local data = json.encode(data)
+	local data, body = http.request('POST', backend_url..method, nil, data)
+	if not body then return false end
+	local result = json.decode(body)["data"]
+	return result
 end
 
 function account_del_link(userid, account_type)
-	local mysql_db = mysql.connect(mysql_host, mysql_port, mysql_user, mysql_password, mysql_db_name)
-	if account_type == "steam64id" then mysql_db:query( string.format("DELETE FROM discord_steam64id_link WHERE userid = '%s'", userid) ) end
-	mysql_db:close()
-end
-
-function account_in_db(userid, account_type)
-	local result
-	local mysql_db = mysql.connect(mysql_host, mysql_port, mysql_user, mysql_password, mysql_db_name)
-	if account_type == "steam64id" then result = mysql_db:query( string.format("SELECT account FROM discord_steam64id_link WHERE userid = '%s'", userid) ) end
-	mysql_db:close()
-	return not result[1] == nil
+	if account_is_account_id(account_type) == false then return false end
+	local method = method..'unlink'
+	data["userid"] = userid
+	data["type"] = account_type
+	local data = json.encode(data)
+	local data, body = http.request('POST', backend_url..method, nil, data)
+	if not body then return false end
+	local result = json.decode(body)["data"]
+	return result
 end
 
 function account_is_account_id(account_type)
-	if account_type == "steam64id" then return true end
+	types = {'steam64id'}
+	for k, v in pairs(types) do
+		if account_type == v then return true end
+	end
+	return false
 end
 
---command_handler.register('accountlink', 'Связывает userid Discord с другими порталами в бд проекта. Типы для связывания: Steam, писать в форме steam64id', '<type> <id>', false, function (msg, argv, args)
-command_handler.register('accountlink', 'Связывает userid Discord с другими порталами в бд проекта. Типы для связывания: Steam, писать в форме steam64id', '<id>', false, function (msg, argv, args)
+command_handler.register('accountlink', 'Привязка других аккаунтов к Discord аккаунту по id. Доступно для связывания: Steam в формате steamid64', '<type> <id>', false, function(msg, argv, args)
 	if msg.guild.id ~= server then return end
-	local userid = msg.author.id
---	if not argv[2] or not account_is_account_id(argv[2]) then msg.channel:send("Нету типа для связывания! Доступные типы: steam64id") return end
-	if not argv[2] then msg.channel:send("Нету айди для связывания! Для типа steam64id нужно указать ваш уникальный идентификатор steam аккаунта используя сайт http://steamid.io") return end
---	if account_in_db(userid, argv[2]) then msg.channel:send("TODO") end
-
---	account_set_link(userid, argv[2], argv[3])
-	account_set_link(userid, 'steam64id', argv[2])
-
-	local output = string.format('К вашему аккаунту привязан %s с таким уникальным идентификатором %s\n', 'steam64id', argv[2])
-	msg.channel:send(output)
+	if not argv[2] then msg:reply('Нету типа для связывания! Доступные типы: Steam') return end
+	if not argv[3] then msg:reply('Нету id для связывания! Напишите после типа') return end
+	local steam64id_aliases = {'steam', 'steamid64', 'steamid', 'steam64id', 'стим', 'стимид'}
+	local type = ''
+	for k, v in pairs(steam64id_aliases) do
+		if argv[2] == v then type = 'steam64id' end
+	end
+	if type == '' then msg:reply(string.format('Не найден тип связывания `%s`. Доступно для связывания: Steam в формате steamid64', argv[2])) return end
+	local result = account_set_link(msg.author.id, type, argv[3])
+	if result ~= 'ok' then msg:reply('Произошла ошибка. Проверьте правильность аргументов') return end
+	msg:reply(string.format('К вашему Discord аккаунту привязан %s с таким уникальным идентификатором: %s', type, argv[3]))
 end)
 
 command_handler.register('adminaccountlink', 'Связывает userid Discord с другими порталами в бд проекта', nil, true, function (msg, argv, args)
 	if msg.guild.id ~= server then return end
+	if not argv[2] then msg:reply('Нету пользователя для связывания! Укажите id или упомяните пользователя') return end
+	if not argv[3] then msg:reply('Нету типа для связывания! Доступные типы: Steam') return end
+	if not argv[4] then msg:reply('Нету id для связывания! Напишите после типа') return end
 	local id = string.gsub(argv[2], '@', '')
 	id = string.gsub(id, '<', '')
 	id = string.gsub(id, '>', '')
 	id = string.gsub(id, '!', '')
-	local userid = cl:getUser(id).id
---	if not argv[3] or account_is_account_id(argv[3]) then msg.channel:send("Нету типа для связывания!") end
---	if not argv[4] then msg.channel:send("Нету айди для связывания!") end
-	if not argv[3] then msg.channel:send("Нету айди для связывания!") end
-
---	account_set_link(userid, argv[3], argv[4])
-	account_set_link(userid, 'steam64id', argv[3])
-
---	local output = string.format('Для %s привязан %s с уникальным идентификатором %s\n', userid, argv[3], argv[4])
-	local output = string.format('Для %s привязан %s с уникальным идентификатором %s\n', userid, 'steam64id', argv[3])
-	msg.channel:send(output)
-end)
-
---command_handler.register('getlinkedaccount', 'Выводит ваши привязанные id. Доступные типы: steam64id', '<type> [mention]', false, function(msg, argv, args)
-command_handler.register('getlinkedaccount', 'Выводит ваши привязанные id. Доступные типы: steam64id', '[mention]', false, function(msg, argv, args)
-	if msg.guild.id ~= server then return end
---	if not argv[2] then
---		msg:reply('Не указан тип аккаунта. Доступные: steam64id')
---		return
---	elseif argv[2] ~= 'steam64id' then
---		msg:reply('Неправильный тип аккаунта. Доступные: steam64id')
---		return
---	end
-
---	if argv[3] then
-	if argv[2] then
-		local id = string.gsub(argv[2], '@', '')
-	        id = string.gsub(id, '<', '')
-	        id = string.gsub(id, '>', '')
-	        id = string.gsub(id, '!', '')
-		if not cl:getUser(id) then msg:reply('Необходимо написать id пользователя или упомянуть его') return end
-	        local userid = cl:getUser(id).id
---		local result = account_get_link(userid, argv[2])
-		local result = account_get_link(userid, 'steam64id')
-		msg:reply(string.format('steam64id пользователя %s: %s', cl:getUser(userid).tag, result))
-		return
+	local user = cl:getUser(id)
+	if not user then msg:reply('Не найден указанный пользователь') return end
+	local steam64id_aliases = {'steam', 'steamid64', 'steamid', 'steam64id', 'стим', 'стимид'}
+	local type = ''
+	for k, v in pairs(steam64id_aliases) do
+		if argv[3] == v then type = 'steam64id' end
 	end
-
---	local result = account_get_link(msg.author.id, argv[2])
-	local result = account_get_link(msg.author.id, 'steam64id')
-
-	msg:reply(string.format('Ваш steam64id: %s', result))
+	if type == '' then msg:reply(string.format('Не найден тип связывания `%s`. Доступно для связывания: Steam в формате steamid64', argv[3])) return end
+	local result = account_set_link(user.id, type, argv[4])
+	if result ~= 'ok' then msg:reply('Произошла ошибка. Проверьте правильность аргументов') return end
+	msg:reply(string.format('Для %s привязан %s с уникальным идентификатором %s\n', user.tag, type, argv[4]))
 end)
 
-command_handler.register('unlink', 'Отвязывание привязанного аккаунта. Доступные типы: steam64id', nil, false, function(msg, argv, args)
+command_handler.register('getlinkedaccount', 'Выводит ваши привязанные id. Доступные типы: Steam в формате steamid64', '<type> [mention]', false, function(msg, argv, args)
 	if msg.guild.id ~= server then return end
-	account_del_link(userid, 'steam64id')
-	msg:reply(string.format('От вашего аккаунта был отвязан steam64id'))
+	if not argv[2] then msg:reply('Нету типа для вывода id! Доступные типы: Steam') return end
+	local steam64id_aliases = {'steam', 'steamid64', 'steamid', 'steam64id', 'стим', 'стимид'}
+	local type = ''
+	for k, v in pairs(steam64id_aliases) do
+		if argv[2] == v then type = 'steam64id' end
+	end
+	if type == '' then msg:reply(string.format('Не найден тип `%s`. Доступно для связывания: Steam в формате steamid64', argv[2])) return end
+	if argv[3] then
+		local id = string.gsub(argv[3], '@', '')
+		id = string.gsub(id, '<', '')
+		id = string.gsub(id, '>', '')
+		id = string.gsub(id, '!', '')
+		local user = cl:getUser(id)
+		if not user then msg:reply('Не найден указанный пользователь') return end
+		local result = account_get_link(user.id, type)
+		if result == 'no account linked with userid' then msg:reply(string.format('У пользователя %s отсутствует привязанный аккаунт типа %s', user.tag, type)) return end
+		msg:reply(string.format('%s пользователя %s: %s', type, user.tag, result))
+	else
+		local result = account_get_link(msg.author.id, type)
+		if result == 'no account linked with userid' then msg:reply(string.format('У вас отсутствует привязанный аккаунт типа %s', type)) return end
+		msg:reply(string.format('Ваш %s: %s', type, result))
+	end
+end)
+
+command_handler.register('unlink', 'Отвязывание привязанного аккаунта. Доступные типы: Steam в формате steamid64', '<type>', false, function(msg, argv, args)
+	if msg.guild.id ~= server then return end
+	if not argv[2] then msg:reply('Не указан тип для отвязывания! Доступные типы: Steam') return end
+	local steam64id_aliases = {'steam', 'steamid64', 'steamid', 'steam64id', 'стим', 'стимид'}
+        local type = ''
+        for k, v in pairs(steam64id_aliases) do
+                if argv[2] == v then type = 'steam64id' end
+        end
+        if type == '' then msg:reply(string.format('Не найден тип для отвязывания `%s`. Доступно для отвязывания: Steam в формате steamid64', argv[2])) return end
+	local result = account_del_link(msg.author.id, type)
+	if result ~= 'ok' then msg:reply('Произошла ошибка. Проверьте правильность аргументов') return end
+	msg:reply(string.format('От вашего аккаунта отвязан %s', type))
 end)
