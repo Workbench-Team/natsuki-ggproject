e733a8e5a43efc87314f4f93c57f4f3d32015b57
ProfessorBrain <anton17082003@gmail.com>
2020-06-14 00:04:51 +0300

    -

--- modules/privilages_events.lua
+++ modules/privilages_events.lua
@@ -8,6 +8,7 @@ command_handler.register('scpsleventsprivilage', 'Работа с привиле
 		id = string.gsub(id, '>', '')
 		id = string.gsub(id, '!', '')
 		local userid = cl:getUser(id).id
+		if not userid then return end
 		privilage_set('events', userid, argv[4])
 		msg:reply('Теперь '..cl:getUser(id).tag..' имеет '..argv[4])
 		cl:getChannel('645301914279608350'):send('<@!'..userid..'> - '..argv[4])
@@ -20,6 +21,7 @@ command_handler.register('scpsleventsprivilage', 'Работа с привиле
 		id = string.gsub(id, '>', '')
 		id = string.gsub(id, '!', '')
 		local userid = cl:getUser(id).id
+		if not userid then return end
 		privilage_delete('events', userid)
 		msg:reply('Удалены все роли с '..cl:getUser(id).tag)
 		cl:getChannel('645301914279608350'):getMessages(100):find(function(msg) local argv = msg.content:split(' ') if argv[1] == '<@!'..userid..'>' then msg:delete() return end end)
