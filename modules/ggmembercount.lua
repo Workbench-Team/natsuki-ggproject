257df8e181d8b9e810c5a40d70820a51d4ec0690
ProfessorBrain <anton17082003@gmail.com>
2020-06-15 01:12:21 +0300

    Count fix

--- modules/ggmembercount.lua
+++ modules/ggmembercount.lua
@@ -2,7 +2,7 @@ cl:on('memberJoin', function(member)
 	if member.guild.id == server then
 		local channel = cl:getChannel('721845451627823185')
 		if channel then
-			channel:setName(string.format('Участников: %s', member.guild.members:count()))
+			channel:setName(string.format('Участников: %s', member.guild.totalMemberCount))
 		end
 	end
 end)
