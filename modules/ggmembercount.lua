26ae26acf96811544a48b7aabbc364a397c9b512
ProfessorBrain <anton17082003@gmail.com>
2020-06-15 22:59:21 +0300

    Update timer cycle

--- modules/ggmembercount.lua
+++ modules/ggmembercount.lua
@@ -1,38 +1,16 @@
-function updateMemberCount(member)
-	if member.guild.id == server then
-		local channel = cl:getChannel('721845451627823185')
-		if channel then
-			channel:setName(string.format('Участников: %s', member.guild.totalMemberCount))
-		end
-	end
-end
-
-cl:on('memberJoin', function(member)
-	updateMemberCount(member)
-end)
-
-cl:on('memberLeave', function(member)
-	updateMemberCount(member)
-end)
-
-
-
-cl:on('voiceChannelLeave', function(member, channel)
-	updateVoiceCount(member)
-end)
-
-cl:on('voiceChannelJoin', function(member, channel)
-	updateVoiceCount(member)
-end)
-
-function updateVoiceCount(member)
-	local channel = cl:getChannel('722163883363532890')
-	if channel then
+timer.setInterval(600000, function()
+	local channel1 = cl:getChannel('722163883363532890')
+	if channel1 then
 		local result = 0
 		cl:getGuild(server).voiceChannels:forEach(
 		function(vc)
 			result = #vc.connectedMembers + result
 		end)
-		channel:setName(string.format('В голосовом: %s', result))
+		channel1:setName(string.format('В голосовом: %s', result))
 	end
-end
+
+	local channel2 = cl:getChannel('721845451627823185')
+	if channel2 then
+		channel2:setName(string.format('Участников: %s', member.guild.totalMemberCount))
+	end
+end)
