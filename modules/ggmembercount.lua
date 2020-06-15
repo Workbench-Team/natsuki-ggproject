bc1a99a967265ba973cbae1a4b1334af30b9823e
ProfessorBrain <anton17082003@gmail.com>
2020-06-15 22:43:27 +0300

    Update voice count and update member count on leave

--- modules/ggmembercount.lua
+++ modules/ggmembercount.lua
@@ -1,8 +1,38 @@
-cl:on('memberJoin', function(member)
+function updateMemberCount(member)
 	if member.guild.id == server then
 		local channel = cl:getChannel('721845451627823185')
 		if channel then
 			channel:setName(string.format('Участников: %s', member.guild.totalMemberCount))
 		end
 	end
+end
+
+cl:on('memberJoin', function(member)
+	updateMemberCount(member)
 end)
+
+cl:on('memberLeave', function(member)
+	updateMemberCount(member)
+end)
+
+
+
+cl:on('voiceChannelLeave', function(member, channel)
+	updateVoiceCount(member)
+end)
+
+cl:on('voiceChannelJoin', function(member, channel)
+	updateVoiceCount(member)
+end)
+
+function updateVoiceCount(member)
+	local channel = cl:getChannel('722163883363532890')
+	if channel then
+		local result = 0
+		cl:getGuild(server).voiceChannels:forEach(
+		function(vc)
+			result = #vc.connectedMembers + result
+		end)
+		channel:setName(string.format('В голосовом: %s', result))
+	end
+end
