d7410999a37a7f5cbba98dbc18aaceb5cb82fd12
ProfessorBrain <anton17082003@gmail.com>
2020-06-19 20:29:21 +0300

    Crash fix

--- modules/nekos.lua
+++ modules/nekos.lua
@@ -5,6 +5,7 @@ command_handler.register('nekos', 'NSFW и SFW картинки с сайта ne
 	neko_type = string.gsub(neko_type, "'", '')
 	local data, body = http.request('GET', 'https://nekos.life/api/v2/img/'..neko_type)
 	if body == nil then msg:reply { embed = { description = 'Проблемы с доступом к API', color = 13632027, author = {name=msg.author.tag,icon_url=msg.author.avatarURL}, footer = {icon_url=cl.user.avatarURL,text=cl.user.tag}, timestamp = disc.Date():toISO('T', 'Z') } } return end
+	if not json.decode(body)['url'] then msg:reply { embed = { description = 'Неверный тег или проблемы с доступом к API', color = 13632027, author = {name=msg.author.tag,icon_url=msg.author.avatarURL}, footer = {icon_url=cl.user.avatarURL,text=cl.user.tag}, timestamp = disc.Date():toISO('T', 'Z') } } return end
 	local url = json.decode(body)['url']
 	if url == nil then msg:reply { embed = { description = 'Неверный тег или проблемы с доступом к API', color = 13632027, author = {name=msg.author.tag,icon_url=msg.author.avatarURL}, footer = {icon_url=cl.user.avatarURL,text=cl.user.tag}, timestamp = disc.Date():toISO('T', 'Z') } } return end
 	if msg.channel.nsfw ~= true then msg:reply { embed = { description = 'Канал должен быть помечен как NSFW', color = 13632027 } } return end
