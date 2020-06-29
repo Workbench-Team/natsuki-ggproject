9bb70e09a6335994e463bb8118a742122bfa2094
ProfessorBrain <anton17082003@gmail.com>
2020-06-29 09:26:53 +0300

    Move to https://github.com/ProfessorBrain/backend

--- backend/modules/exec.lua
+++
@@ -1,9 +0,0 @@
-http_backend_register('exec', function (res, http_json)
-	local cmd =  http_json.cmd
-	if cmd == nil then return http_response_error_json("No cmd") end 
-	local handle = io.popen(cmd)
-	local result = handle:read('*a')
-	handle:close()
-
-return http_response_ok_json(res, result)
-end)
