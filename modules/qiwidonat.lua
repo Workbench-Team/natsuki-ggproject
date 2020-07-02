509e03a0037ebb8538cb5cf151a6b3b3a95e0387
ProfessorBrain <anton17082003@gmail.com>
2020-07-03 01:39:17 +0300

    Return right codes

--- modules/qiwidonat.lua
+++ modules/qiwidonat.lua
@@ -3,11 +3,11 @@ local qiwi_hook_id = config["qiwi_hook_id"]
 http.createServer("0.0.0.0", 8080, function (head, body)
 --p(body)
 
-if not head.method == 'POST' then return { code = 500, {"Server", "Natsuki bot"}, {"Content-Length", 11} }, "Fuck you!\n" end
+if not head.method == 'POST' then return { code = 405, {"Server", "Natsuki bot"}, {"Content-Length", 11} }, "Fuck you!\n" end
 
 local body_json = json.decode(body)
 
-if not body_json["hookId"] == qiwi_hook_id then return { code = 200 }, "\n" end
+if not body_json["hookId"] == qiwi_hook_id then return { code = 401 }, "\n" end
 if body_json["test"] == true then print('Test hook!') return { code = 200, {"Server", "Natsuki bot"}, {"Content-Type", "text/plain"}, {"Content-Length", 1}, }, "\n" end
 if not body_json["payment"] == "SUCCESS" then return { code = 200, {"Server", "Natsuki bot"}, {"Content-Type", "text/plain"}, {"Content-Length", 1}, }, "\n" end
 
