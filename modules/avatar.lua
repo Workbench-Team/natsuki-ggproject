c6dca61a2f589a90fa0dfdc5c03ae2dc2954d160
canada <34052170+NotSoClassy@users.noreply.github.com>
2020-07-23 03:43:18 -0400

    Update avatar.lua

--- modules/avatar.lua
+++ modules/avatar.lua
@@ -1,6 +1,6 @@
 command_handler.register('avatar', 'Получить ссылку и изображение на аватарке', nil, false, function (msg, argv, args)
 	if argv[2] then
-		local user = msg.mentionedUsers.first
+		local user = msg.mentionedUsers.first or cl:getUser(argv[2])
 		if user then
 			msg:reply { embed = { description = '[Ссылка на изображение]('..user:getAvatarURL(1024)..')', image = { url = user:getAvatarURL(1024) } } }
 		end
