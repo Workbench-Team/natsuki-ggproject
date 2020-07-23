2ed92281eab6a6158a270def97742f033f877d81
canada <34052170+NotSoClassy@users.noreply.github.com>
2020-07-23 03:33:13 -0400

    just realized thats user object

--- modules/avatar.lua
+++ modules/avatar.lua
@@ -1,7 +1,6 @@
 command_handler.register('avatar', 'Получить ссылку и изображение на аватарке', nil, false, function (msg, argv, args)
 	if argv[2] then
-		local id = msg.mentionedUsers.first.id
-		local user = cl:getUser(id)
+		local user = msg.mentionedUsers.first
 		if user then
 			msg:reply { embed = { description = '[Ссылка на изображение]('..user:getAvatarURL(1024)..')', image = { url = user:getAvatarURL(1024) } } }
 		end
