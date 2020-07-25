cb5f4f5c77beb866d214761d37e4cfe16beaffbd
ProfessorBrain <anton17082003@gmail.com>
2020-07-25 22:39:20 +0300

    Admin added

--- groups.lua
+++ groups.lua
@@ -2,5 +2,5 @@ groups = {
 	['277020687716319233'] = 'owner', -- ProfessorBrain
 	['466865702394134528'] = 'admin', -- Mr0maks
 	['593382936569446402'] = 'admin', -- Dryzhok
-	['443349324412485633'] = 'admin', -- JASIO
+        ['543516044904628224'] = 'admin', -- ProstoChelovek
 }
