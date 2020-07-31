0e1fa157ed4831af2311752267e919c99a92d2f9
canada <34052170+NotSoClassy@users.noreply.github.com>
2020-07-31 08:34:10 -0400

    shorter (#10)

--- modules/botserverscount.lua
+++ modules/botserverscount.lua
@@ -1,6 +1,6 @@
 timer.setInterval(15000, function()
 	local count = cl.guilds:count()
 	coroutine.wrap(function()
-		cl:setGame{type = 3, name = pref..'help | Servers: '..tostring(cl.guilds:count())}
+		cl:setGame{type = 3, name = pref..'help | Servers: '..#cl.guilds}
 	end)()
 end)
