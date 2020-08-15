6434c59a3bf0a70300728c549d900c5d91585dba
canada <34052170+NotSoClassy@users.noreply.github.com>
2020-08-15 12:28:49 -0400

    Update choose.lua

--- modules/choose.lua
+++ modules/choose.lua
@@ -2,7 +2,7 @@ command_handler.register('choose', 'change this', nil, false, function (msg, arg
   local vsargs = args:split('vs')
   if vsargs[1] and vsargs[2] then
 		vsargs[1] = vsargs[1]:gsub(argv[1], '')
-		message:reply('The choice is: '..cargs[math.random(#cargs)]) 
+		message:reply('The choice is: '..vsargs[math.random(#vsargs)]) 
 	else
 		message:reply('Please put at least two arguments to choose from.')
 	end
